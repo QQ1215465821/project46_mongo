a0a25a53857533ff6645a011eccfac91af42627c
刘鹏 <1215465821@qq.com>
2024-06-14 03:15:44 +0800

    111

---
+++ src/main/java/com/example/project46_mongo/Project46MongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.project46_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project46MongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project46MongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project46_mongo/Project46MongoApplicationTests.java
@@ -0,0 +1,70 @@
+package com.example.project46_mongo;
+
+import com.example.project46_mongo.mongo.Book;
+import com.example.project46_mongo.mongo.User;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+class Project46MongoApplicationTests {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    void contextLoads() {
+//        Book book = new Book();
+//        book.setId(11);
+//        book.setName("testMongoDB");
+//        book.setType("testMongoDB");
+//        book.setDescription("testMongoDB");
+//        mongoTemplate.save(book);
+
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setId(i);
+            user.setName("testMongoDB");
+            user.setType("testMongoDB");
+            user.setDescription("testMongoDB");
+            user.setAge(10*i);
+            User save = mongoTemplate.save(user);
+            System.out.println("save = " + save);
+        }
+
+    }
+    @Rollback(value = false)
+    @Transactional(rollbackFor = Exception.class)
+    @Test
+    void contextLoads2() {
+        User user = new User();
+        user.setId(1);
+        user.setName("testMongoDB22");
+        user.setType("testMongoDB");
+        user.setDescription("testMongoDB");
+        User save = mongoTemplate.save(user);
+        mongoTemplate.save(user, "user2");
+//        int a = 1/0;
+    }
+    @Test
+    void contextLoads3() {
+        List<User> userList = mongoTemplate.findAll(User.class);
+        System.out.println("userList = " + userList);
+
+        System.out.println("user = " + mongoTemplate.findById(1, User.class));
+        Query query = new Query();
+        query.addCriteria(Criteria.where("age").gte(20).lte(30));
+        query.addCriteria(Criteria.where("age").gte(20).lte(30));
+        System.out.println("userList = " + mongoTemplate.find(query, User.class));
+
+    }
+
+}
